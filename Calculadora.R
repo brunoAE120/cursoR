a2ea17e717844211ae1f6c6cea22284bfed0c4e4
brunoAE120 <brunnoribeirinho@hotmail.com>
2022-12-10 17:33:29 -0300

    Adicionadas as operações básicas

--- Calculadora.R
+++ Calculadora.R
@@ -1,1 +1,19 @@
-## Usando R como calculadora
+#Usando R como calculadora
+
+#Para fazer rodar uma linha de conta, aperte Ctrl+Enter na própria linha
+
+4+6 #Soma
+4*5 #Multiplicação
+4/2 #Divisão
+7-9 #Subtração
+2*3-5 #Expressão númerica
+6%%3 #Resto
+6%/%3 #Parte inteira
+3^2 #Potência
+3**3 #Potência
+log(2) #Logaritmo
+factorial(5) #Fatorial
+abs(-8) #Módulo
+sin(5) #Seno
+cos(5) #Cosseno
+sqrt(9) #Raiz quadrada
